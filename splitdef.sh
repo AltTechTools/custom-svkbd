d187187b12e75ac2be014c3217ed077f38a069f8
fssSGJ <fsstweeter@gmail.com>
2021-11-02 17:37:59 +0100

    worked on split script a little

--- splitdef.sh
+++ splitdef.sh
@@ -4,12 +4,22 @@ folder="$1" #"keys_en"
 #cd "$folder" || exit 1
 
 inputfile="layout.$folder.h"
-mainpath="."
+mainpath="./$folder"
 path="$mainpath"
 isoverlay=0
 overlayonkey=""
+iskey=0
+keylinename=""
+keylineno=0
+islayersname=0
+islayerkey=0
+isbuttonmod=0
+[ -d "$folder" ] || mkdir "$folder"
 while read -r line
 do
+if [ "$line" = "" ]; then
+ echo "empty skipped" > /dev/null
+else
 #	title=$(printf "%s" "${line}" | awk '{print $1}' FS=";")
 #        otherchars=$(printf "%s" "${line}" | awk '{print $2}' FS=";")
 #        key=$(printf "%s" "${line}" | awk '{print $3}' FS=";")
@@ -17,24 +27,107 @@ do
 #	printf "%s\n" "        { ${title}, ${otherchars}, ${key}, ${sizeing} }, " >>  $outputfile
 	keyline=$(echo "$line" | grep -c "^static Key keys_")
 	overlayline=$(echo "$line" | grep -c "^static Key overlay")
+	if [ "$line" = "};" ]; then
+		isoverlay=0
+		iskey=0
+		keylinename=""
+		keylineno=0
+		isbuttonmod=0
+		islayerkey=0
+	fi
+	if [ "$line" = "static char* layer_names[LAYERS] = {" ]; then
+		islayersname=1
+		isoverlay=0
+		iskey=0
+		islayerkey=0
+		isbuttonmod=0
+	fi
+
+	if [ "$line" = "static Key* available_layers[LAYERS] = {" ]; then
+		islayerkey=1
+		islayersname=0
+		isoverlay=0
+		iskey=0
+		isbuttonmod=0
+	fi
+	if [ "$line" = "Buttonmod buttonmods[] = {" ]; then
+		isbuttonmod=1
+		islayerkey=0
+		islayersname=0
+		isoverlay=0
+		iskey=0
+	fi
+
+# these 2 eventually need to be connected
+	if [ $islayersname -eq 1 ]; then
+		echo "$islayersname"
+	fi
+	if [ $islayersname -eq 1 ]; then
+		echo "$islayersname"
+	fi
+#this only onece
+	if [ $isbuttonmod -eq 1 ]; then
+		if [ "$line" != "Buttonmod buttonmods[] = {" ]; then
+			echo "$line"
+		fi
+	fi
+
 	if [ $overlayline -eq 1 ]; then
 		isoverlay=1
+		iskey=0
 #		outputname=$(echo "$line")
 		outputname="_overlay"
-		echo "$outputname"
-		[ -d "$outputname" ] || mkdir "$outputname"
+#		echo "$outputname"
 		path="$mainpath/$outputname"
+		[ -d "$path" ] || mkdir -p "$path"
 	fi
 	if [ $keyline -eq 1 ]; then
 		# && echo "$line"
+		iskey=1
 		isoverlay=0
 		outputname=$(echo "$line" | awk '{print $1}' FS="[" | sed 's/static Key //')
-		echo "$outputname"
-		[ -d "$outputname" ] || mkdir "$outputname"
-		#cd "$outputfile"
+#		echo "$outputname"
 		path="$mainpath/$outputname"
-		#cd ..
+		[ -d "$path" ] || mkdir -p "$path"
 	fi
+
+
+#        { "1", "!", XK_1, 1 },
+#        { 0 }, /* New row */
+
+
+	if [ $iskey -eq 1 ]; then
+		if [ $keyline -lt 1 ]; then
+			cleanedline=$(echo "$line" | sed 's/ //g'   | sed 's/{//g'   | sed 's/}//g')
+                        primsymb=$(echo "${cleanedline}"|awk '{print $1}' FS=",")
+                        altsymb=$(echo "${cleanedline}"|awk '{print $2}' FS=",")
+                        keysym=$(echo "${cleanedline}"|awk '{print $3}' FS=",")
+                        keyscaling=$(echo "${cleanedline}"|awk '{print $4}' FS=",")
+			#keylinename=""
+			#keylineno=0
+#prob faulty, only overlay leaves out the first char 
+			if [ "$keylinename" = "" ]; then
+				keylineno=$(expr $keylineno + 1)
+				keylinename="ln_$keylineno"
+#				echo "$keylinename"
+				[ -e  "$path/$keylinename" ] && rm "$path/$keylinename"
+			fi
+#			else
+				endcount=$(echo "$line" | grep -c "\{ 0 \},")
+				if [ $endcount -gt 0 ]; then
+					#keylinename=""
+					#keylineno=$keylineno+1
+					keylinename=""
+                                else
+					#echo "$primsymb;$altsymb;$keysym;$keyscaling"
+#					echo "$primsymb;$altsymb;$keysym;$keyscaling"
+                            		echo "$primsymb;$altsymb;$keysym;$keyscaling" >> "$path/$keylinename"
+                                fi
+#			fi
+
+		fi
+	fi
+
 	if [ $isoverlay -eq 1 ]; then
 		if [ $overlayline -lt 1 ]; then
 			#echo "some overlayline $line"
@@ -44,13 +137,20 @@ do
 			keysym=$(echo "${cleanedline}"|awk '{print $3}' FS=",")
 			#echo "$primsymb-$altsymb-$keysym"
 			if [ "$overlayonkey" = "" ]; then
-				echo "$keysym"
+				#echo "$keysym"
 				overlayonkey="$keysym"
-			fi
-			if [ "$keysym" = "XK_Cancel" ]; then
-				overlayonkey=""
+				[ -e  "$path/$overlayonkey" ] && rm "$path/$overlayonkey"
+				touch "$path/$overlayonkey"
+			else
+				if [ "$keysym" = "XK_Cancel" ]; then
+					overlayonkey=""
+				else
+					#echo "$overlayonkey -> $keysym"
+					echo "$primsymb;$altsymb;$keysym" >> "$path/$overlayonkey"
+				fi
 			fi
 		fi
 ### do not go by 0,0 , go by first element aftr xk_cancel		
 	fi
+fi
 done < "$inputfile"
